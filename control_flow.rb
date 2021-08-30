91ca7947585045db96b80bff210720c5edb5f8eb
Anna Wilson <ajoyna01@gmail.com>
2021-08-30 11:24:08 -0500

    Attempted but still failing some tests

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,40 @@
 def admin_login(username, password)
-  # your code here
+if username == admin && password == 12345 then "Access granted"
+if username == ADMIN && password == 12345 then "Access granted"
+else "Access denied"
 end
 
+
+
 def hows_the_weather(temperature)
-  # your code here
+ if temperature  <40 then "Brisk!"
+  # if temperature  >40 && <65 then "It's a little chilly out there!"
+  if temperature  >85 then "It's too dang hot out there!"
+else "It's perfect out there!"
 end
 
 def fizzbuzz(num)
-  # your code here
+  if num % 3 then "Fizz"
+  if num % 5 then "Buzz"
+  if numb % 3 && % 5 then "FizzBuzz"
+  else num 
 end
 
 def calculator(operation, num1, num2)
-  # your code here
-end
+ case operation
+  when + 
+    calculator = num1 + num2
+   
+  when - 
+    calculator = num2 - num1
+
+  when * 
+    calculator = num1 * num2  
+
+  when / 
+    calculator = num1 / num2 
+  else 
+  "Invalid operation!" 
+  return nil
+  end 
 
